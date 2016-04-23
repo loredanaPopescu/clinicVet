83253a3f500437a997f3b1c7a05437da52efab4d
loredana <loredana@falcontrading.ro>
2016-04-23 16:35:22 +0200

    add capcha

---
+++ clinicVet/Registration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clinicVet
+{
+    public partial class Registration : Form
+    {
+        public Registration()
+        {
+            InitializeComponent();
+            loadcaptchaimage();
+        }
+
+        int number = 0;
+        private void loadcaptchaimage() {
+            Random r1 = new Random();
+            number = r1.Next(100,10000);
+            var image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            var font = new Font("TimesNewRoman", 25, FontStyle.Bold, GraphicsUnit.Pixel);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawString(number.ToString(), font, Brushes.ForestGreen, new Point(0, 0));
+            pictureBox1.Image = image;
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Login f1 = new Login();
+            f1.Show();
+            this.Hide();
+        }
+
+        private void Registration_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            loadcaptchaimage();
+            captcha.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (captcha.Text == number.ToString())
+            {
+                MessageBox.Show("Match Text with capcha!");
+            }
+            else {
+                MessageBox.Show("Does not match with capcha!");
+                loadcaptchaimage();
+                captcha.Text = "";
+
+            }
+        }
+    }
+}
